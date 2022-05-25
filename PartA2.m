2b6cb852b54dbd59897e74a46e79876565a7801d
Matthew-Hatton <94123416+Matthew-Hatton@users.noreply.github.com>
2022-05-25 17:43:28 +0100

    Update PartA2.m

--- PartA2.m
+++ PartA2.m
@@ -1,5 +1,4 @@
-%Move all into same document
-%Most dangerous age class is (1)
+#We analyse a given Leslie Matrix to produce plots of the probability of death of sea turtles
 ageclass = [1,2,3,4,5,6,7];
 %To get probdeath do 1 - (g_i+p_i)
 probdeath = [0.9,0.5,0.3,0.3,0.2,0.1,0.1];
